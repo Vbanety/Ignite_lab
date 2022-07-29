c6cc5a80fa545f77411502284df5eee8d3ae15b3
Vinicius Batista <vbanetyy@gmail.com>
2022-07-29 19:37:59 -0300

    Fixed renderizing from home page to mobile devices

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
+import { Footer } from "../components/Footer";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
@@ -28,18 +29,20 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
-          <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+      <div className="w-full max-w-[1100px] flex flex-col items-center justify-between mt-20 mx-auto md:flex-row">
+        <div className="m-0 w-full md:max-w-[640px] md:m-auto">
+          <div className="flex justify-center w-full m-auto md:mb-4 md:w-auto md:justify-start">
+            <Logo />
+          </div>
+          <h1 className="m-auto p-2 text-center md:text-left text-[1.9rem] md:mt-8 md:text-[2.5rem] leading-tight md:p-0">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong> JS
           </h1>
-          <p className="mt-4 text-gray-200 leading-tight">
+          <p className="m-auto p-2 mb-4 text-center md:text-left md:mt-4 text-gray-200 leading-tight md:p-0">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="w-full p-8 bg-gray-700 border border-gray-500 rounded md:w-auto">
           <strong className="text-2xl mb-6 block">Escreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} action="" className="flex flex-col gap-2 w-full" method="post">
@@ -67,6 +70,7 @@ export function Subscribe() {
         </div>
       </div>
       <img src="/src/assets/code-mockup.png" alt="mockup" className="mt-10" />
+      <Footer />
     </div>
   )
 }
